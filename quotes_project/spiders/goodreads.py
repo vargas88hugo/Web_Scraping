f19617394b313a8d21f4475c8473c2b1f3e318cc
Hugo Vargas <vargas88hugo@gmail.com>
2019-12-09 17:47:22 -0500

    [Feature] changed the logic of the file, added ItemLoader that implemented components to the data generated. Now the data generated is infinite or end when it finds the last page, this is configured with the next button

--- quotes_project/spiders/goodreads.py
+++ quotes_project/spiders/goodreads.py
@@ -1,4 +1,13 @@
+"""
+At the beginning of the proyect a logic without components was used. From this 
+commit that logic is changed with Items components and ItemLoader mechanism. The
+Items are explained in items.py file and the ItemLoeader is the way it connects the
+scrapy core with the fields of the Item file.
+"""
+
 import scrapy
+from scrapy.loader import ItemLoader # ItemLoader provides a mechanism for populating scraped items
+from demo_project.items import QuoteItem # Import class from the item file
 
 class GoodReadsSpider(scrapy.Spider):
     # identity
@@ -18,9 +27,19 @@ class GoodReadsSpider(scrapy.Spider):
         """
         This method is to extract links and return the requests
         """
-        for quote in response.selector.xpath("//div[@class='quote']"): # here starts the implementation of xpath
-            yield {
-                'text': quote.xpath(".//div[@class='quoteText']/text()[1]").extract_first(), # extract_first() method extract only the first match and can be replaced with get() 
-                'author': quote.xpath(".//span[@class='authorOrTitle']/text()").extract_first(),
-                'tags': quote.xpath(".//div[@class='greyText smallText left']/a/text()").extract() # extrac() method extract all the matches and can be replaced with getall() 
-            }
+        for quote in response.xpath("//div[@class='quote']"): # here starts the implementation of xpath
+            loader = ItemLoader(item=QuoteItem(), selector=quote, response=response)
+            loader.add_xpath('text', ".//div[@class='quoteText']/text()[1]")
+            loader.add_xpath('author', ".//span[@class='authorOrTitle']/text()")
+            loader.add_xpath('tags', ".//div[@class='greyText smallText left']/a/text()")
+            yield loader.load_item()
+
+
+        """
+        Here is used the next buttom of the page to iterate until the last page
+        """
+        next_page = response.xpath("//a[@class='next_page']/@href").extract_first()
+
+        if next_page is not None:
+            next_page_link = response.urljoin(next_page)
+            yield scrapy.Request(url=next_page_link, callback=self.parse)
